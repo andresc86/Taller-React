52e2f870cd96e5b8f27b244b503ed8e67ca0fdd3
Andres <andres.cifuentesgu@amigo.edu.co>
2025-10-15 07:30:39 -0500

    feact: integracion andres

---
+++ tallereact/src/components/Andres/IntegracionAndres.jsx
@@ -0,0 +1,8 @@
+export default function Saludo() {
+  return (
+    <div style={{ padding: 12, border: '1px solid #ddd', borderRadius: 8 }}>
+      <h2>Primer Integracion para un proyecto de "Desarrollo Colaborativo y Despliegue Continuo con React y Netlify"</h2>
+      <p>Andres :D</p>
+    </div>
+  );
+}
